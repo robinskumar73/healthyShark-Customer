599765c100916937f8f93ed32d10f30574be5c26
robins gupta <robinskumar73@gmail.com>
2015-01-29 14:28:34 +0530

    First commit of customer app

---
+++ scripts/index.js
@@ -0,0 +1,11 @@
+'use strict';
+/*global window */
+
+
+var app = window.app || {};
+
+$( document ).ready(function() {
+    var docHeight = $(document).height();
+    //Now adjusting icons according to the screen height..
+    
+});
